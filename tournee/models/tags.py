f06bd1adbd6d5d8735d323445f3f20f1d1265ee3
fa-odoo <fa@syentys.fr>
2022-05-24 09:29:18 +0100

    commit

--- tournee/models/tags.py
+++ tournee/models/tags.py
@@ -27,8 +27,8 @@ class Tags(models.Model):
 			if rec.tag_line_ids:
 				tag_line_ids = rec.tag_line_ids.filtered(lambda r: r.scan_date)
 				if tag_line_ids:
-					last_date_scan = tag_line_ids.sorted('scan_date')[-1]
-			rec.last_date_scan = last_date_scan.scan_date
+					last_date_scan = tag_line_ids.sorted('scan_date')[-1].scan_date
+			rec.last_date_scan = last_date_scan
 
 	@api.depends('navire_id', 'pont', 'designation', 'numero', 'lot', 'couple' )
 	def compute_tags_name(self):
