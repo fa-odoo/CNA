2c9a45b38f8f3734b6a50bf8cbd58d3a23530e76
ma@syentys.fr <ma@syentys.fr>
2023-01-13 13:12:29 +0100

    [ADD] transit time graph

--- tournee/report/transit_time_graph_xlsx.py
+++ tournee/report/transit_time_graph_xlsx.py
@@ -10,14 +10,18 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import io
 import datetime
+
+
 class TransitTimeGraphXlsx(models.AbstractModel):
     _name = 'report.tournee.transit_time_graph_xlsx'
     _inherit = 'report.report_xlsx.abstract'
 
     def generate_xlsx_report(self, workbook, data, obj):
+        week_data = []
+        label_week_data = []
+        hour_data = []
         key = 1
         cr = self.env.cr
-        print("data['navire_ids']",data['navire_ids'])
         navire_ids = str(data['navire_ids']).replace('[', '(').replace(']', ')')
         cr.execute(
             """
@@ -25,41 +29,29 @@ class TransitTimeGraphXlsx(models.AbstractModel):
             WHERE navire_id IN {navire_ids}
             ORDER BY navire_id
             """.format(navire_ids=navire_ids))
-        tag_ids = [val[0]for val in cr.fetchall()]
-        print('===========tag_ids', tag_ids)
+        tag_ids = [val[0] for val in cr.fetchall()]
         start_date = fields.Date.from_string(data['start_date'])
         end_date = fields.Date.from_string(data['end_date'])
-
-        # import datetime module
-        week_data = []
-        label_week_data = []
-        hour_data = []
 
         end = start_date + timedelta(days=6)
         start = start_date
         if end <= end_date:
             while end <= end_date:
-                hour_res, res_label = self.get_avg_time(tag_ids, start, end)
+                hour_res, res_label = self.get_avg_time(tag_ids, start, end, cr)
                 if hour_res != False:
-                    print('hour_res', hour_res)
                     hour_data.append(hour_res)
                     label_week_data.append(res_label)
                     week_data.append(key)
                     key += 1
                 start = end + timedelta(days=1)
                 end = start - timedelta(days=start.weekday()) + timedelta(days=6)
-
         else:
-            hour_res, res_label = self.get_avg_time(tag_ids, start, end_date)
+            hour_res, res_label = self.get_avg_time(tag_ids, start, end_date, cr)
             if hour_res != False:
                 hour_data.append(hour_res)
                 label_week_data.append(res_label)
                 week_data.append(key)
 
-        print('=======================', key)
-        print('label_week_data', label_week_data)
-        print('hour_data', hour_data)
-        print('week_data', week_data)
         # specify a date to use for the times
         zero = datetime.datetime(2018, 1, 1)
         time = [zero + t for t in hour_data]
@@ -78,29 +70,14 @@ class TransitTimeGraphXlsx(models.AbstractModel):
         sheet.write(3, 4, "Temps de passage")
         sheet.insert_image(4, 4, "image.png", {'image_data': buf})
 
-    def get_avg_time(self, tag_ids, start_date, end_date):
+    def get_avg_time(self, tag_ids, start_date, end_date, cr):
         diff_time = False
-        default_domain = []
-        if start_date:
-            default_domain.append(('scan_date', '>=', start_date))
-        if end_date:
-            default_domain.append(('scan_date', '<=', end_date))
-        for tag_id in tag_ids:
-            before_tag_line_id = False
-            domain = default_domain.copy()
-            domain.append(('tag_id', '=', tag_id))
-            tag_line_id = self.env['task.tags.line'].sudo().search(domain, order='scan_date desc', limit=2)
-            print('domain ',domain)
-            print('ltag_line_id ',tag_line_id)
-            print('len(tag_line_id) ',len(tag_line_id))
-            if len(tag_line_id) >= 2:
-                print('============================= ')
-                if diff_time:
-                    diff_time += tag_line_id[0].scan_date - tag_line_id[1].scan_date
-                else:
-                    diff_time = tag_line_id[0].scan_date - tag_line_id[1].scan_date
-        if diff_time:
-            diff_time = diff_time / len(tag_ids)
+        cr.execute(
+            "SELECT SUM(avg_date_table.avg_date) FROM ( SELECT tag_id, min(scan_date) as start_date, max(scan_date) as end_date, max(scan_date) - min(scan_date) as avg_date FROM ( SELECT tag_id, scan_date FROM (SELECT ROW_NUMBER() OVER (PARTITION BY tag_id ORDER BY scan_date DESC  NULLS LAST) AS r, t.* FROM task_tags_line t) line WHERE line.r <= 2 AND tag_id IN %s AND DATE(scan_date) >= DATE(%s) AND DATE(scan_date) <= DATE(%s)) scanned_date GROUP BY scanned_date.tag_id) avg_date_table",
+            (tuple(tag_ids), str(start_date), str(end_date)))
+        res = self._cr.fetchone()
+        if res[0]:
+            diff_time = res[0] / len(tag_ids)
         return diff_time, self.get_label(str(diff_time).split('.')[0], end_date)
 
     def get_label(self, diff_time, end_date):
