678e70059be090087ee651bb0537d61161ce8141
ma@syentys.fr <ma@syentys.fr>
2023-01-18 14:54:23 +0100

    [IMP] report excluded week

--- tournee/report/navire_transit_time_xlsx.py
+++ tournee/report/navire_transit_time_xlsx.py
@@ -3,6 +3,7 @@
 from odoo import models, fields
 from dateutil.rrule import rrule, MONTHLY
 import random
+import datetime
 
 
 class NavireTransitTimeXlsx(models.AbstractModel):
@@ -14,15 +15,23 @@ class NavireTransitTimeXlsx(models.AbstractModel):
         i = 1
         docs = []
         cr = self.env.cr
+        start_date = fields.Date.from_string(data['date_start'])
+        end_date = fields.Date.from_string(data['date_end'])
+        exclude_date = []
+        while start_date <= end_date:
+            if start_date.weekday() in [5, 6]:
+                exclude_date.append(str(start_date))
+            start_date += datetime.timedelta(days=1)
         th_format = workbook.add_format({'bold': True, 'border': 1, 'align': 'center', 'valign': 'vcenter', 'bg_color': '#D9D9D9'})
         td_format = workbook.add_format({'border': 1, 'align': 'center', 'valign': 'vcenter', 'text_wrap': True})
         tag_ids = self.env['tags.tags'].sudo().search([('navire_id', 'in', data['navire_ids'])], order='navire_id')
 
         cr.execute("SELECT tag_id, max(scan_date) as end_date, max(scan_date) - min(scan_date) as avg_date "
                    "FROM (SELECT tag_id, scan_date FROM task_tags_line "
-                   "WHERE tag_id IN %s AND DATE(scan_date) >= DATE(%s) AND DATE(scan_date) <= DATE(%s) ORDER BY scan_date DESC  NULLS LAST) scan_res "
+                   "WHERE tag_id IN %s AND DATE(scan_date) NOT IN %s AND DATE(scan_date) >= DATE(%s) AND DATE(scan_date) <= DATE(%s) ORDER BY scan_date DESC  NULLS LAST) scan_res "
                    "GROUP BY tag_id",
-                   (tuple(tag_ids.ids), data['date_start'], data['date_end']))
+                   (tuple(tag_ids.ids), tuple(exclude_date), data['date_start'], data['date_end']))
+
         tags_line = {x[0]: [x[1], x[2]] for x in cr.fetchall()}
         for tag_id in tag_ids:
             tag_line = False
@@ -52,4 +61,4 @@ class NavireTransitTimeXlsx(models.AbstractModel):
             sheet.write(i, 2, doc[2].strftime('%d/%m/%Y %H:%M') if doc[2] != '' else '', td_format)
             sheet.write(i, 3, doc[3], td_format)
             i += 1
-        sheet.write(i, 3, "Moyenne = {}".format(str(diff_time/len(tag_ids)).split('.')[0]), td_format)
+        sheet.write(i, 3, "Moyenne = {}".format(str(diff_time/len(docs)).split('.')[0]), td_format)
